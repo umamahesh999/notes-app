04f054017a4ebf4e46ed6db36eec904bab5ad004
Pinninti Umamaheswara Rao <umamahesh320@gmail.com>
2017-03-11 11:09:23 +0000

    Implement listing functionality

--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log("Starting notes.js");
-
 const fs = require('fs');
 
 var fetchNotes = () => {
@@ -32,7 +30,7 @@ var addNote = (title, body) => {
 }
 
 var getAll = () => {
-	console.log("Listing all notes");
+	return fetchNotes();
 }
 
 var getNote = (title) => {
